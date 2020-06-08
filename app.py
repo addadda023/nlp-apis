456c906896f0e0cc8f7f82518c2dab241ba99698
adda <avsar+dahal@gmail.com>
2020-06-07 19:43:10 -0700

    Changed uvicorn run to 'app:app'

--- app.py
+++ app.py
@@ -60,4 +60,4 @@ async def sentiment_request(request: Request):
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000, log_level='info')
+    uvicorn.run('app:app', host='0.0.0.0', port=8000, log_level='info')
